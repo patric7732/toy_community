6c9b11f481f3b35616a3b31dd36e6573ae71f5e2
patric7732 <patric7732@gmail.com>
2024-06-07 21:17:13 +0900

    feat: 기본 폴더 설정

---
+++ src/main/java/org/example/toy_restboard/ToyRestboardApplication.java
@@ -0,0 +1,13 @@
+package org.example.toy_restboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRestboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyRestboardApplication.class, args);
+    }
+
+}
